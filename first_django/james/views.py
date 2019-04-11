802dfc5ca33d4313088dd790b7004deafbff3578
James de Winton <james.dewinton@gmail.com>
2019-04-11 15:14:59 +0100

    update detail method

--- first_django/james/views.py
+++ first_django/james/views.py
@@ -10,7 +10,11 @@ def index(request):
     return render(request, 'james/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'james/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
