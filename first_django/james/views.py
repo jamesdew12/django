0befb3c3e34d2ab028fa1289df5afa4f5f0315b3
James de Winton <james.dewinton@gmail.com>
2019-04-11 14:41:09 +0100

    cleaner code

--- first_django/james/views.py
+++ first_django/james/views.py
@@ -6,11 +6,8 @@ from django.template import loader
 
 def index(request):
     latest_question_list = Question.objects.order_by('-date')[:5]
-    template = loader.get_template('james/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'james/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
