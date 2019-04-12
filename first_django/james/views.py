9eaea2fba3780c591155f0274d8e0f22b33e630c
James de Winton <james.dewinton@gmail.com>
2019-04-12 10:56:12 +0100

    get object or 404 shortcut imported

--- first_django/james/views.py
+++ first_django/james/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
 
 def index(request):
